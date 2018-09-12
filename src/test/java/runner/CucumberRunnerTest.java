a56fefea54d9792d31b9d77becec522107cb31b4
Sasa Gatej <sasa.gatej@endava.com>
2018-09-12 10:53:18 +0300

    More refactoring and moving

--- src/test/java/runner/CucumberRunnerTest.java
+++ src/test/java/runner/CucumberRunnerTest.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "features", glue = { "stepDefinition" }, tags = { "~@ignore", "~@skip" }, plugin = {
+@CucumberOptions(features = "src/test/resources/features", glue = { "stepDefinition" }, tags = { "~@ignore", "~@skip" }, plugin = {
 		"html:target/cucumber-html-report", "json:target/cucumber.json", "progress",
 		"usage:target/cucumber-usage.json", "junit:target/cucumber-results.xml" })
 public class CucumberRunnerTest {
