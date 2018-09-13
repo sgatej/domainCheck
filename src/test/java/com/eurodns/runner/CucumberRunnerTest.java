373ca01763b11551c5ef1deb1e7aa3bf3a4a84a5
Sasa Gatej <sasa.gatej@endava.com>
2018-09-13 14:34:51 +0300

    Fixed tests

--- src/test/java/com/eurodns/runner/CucumberRunnerTest.java
+++ src/test/java/com/eurodns/runner/CucumberRunnerTest.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features", glue = { "stepdefinition" }, tags = { "~@ignore", "~@skip" }, plugin = {
+@CucumberOptions(features = "src/test/resources/features", glue = { "com.eurodns.stepdefinition" }, tags = { "~@ignore", "~@skip" }, plugin = {
 		"html:target/cucumber-html-report", "json:target/cucumber.json", "progress",
 		"usage:target/cucumber-usage.json", "junit:target/cucumber-results.xml" })
 public class CucumberRunnerTest {
